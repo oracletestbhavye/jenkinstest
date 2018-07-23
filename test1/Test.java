375f4e17ff3a1cf02c813bfe2ac551c8b67cf3ea
Bhavya Sankhla <bhavya.sankhla@oracle.com>
2018-07-23 15:28:42 +0530

    added 2 folders

---
+++ test1/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] arg) {
+        System.out.println("I rock.");
+        System.out.println("Test 1 rock.");
+    }
+}
---
+++ test2/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] arg) {
+        System.out.println("I rock.");
+        System.out.println("test 2 rock.");
+    }
+}
